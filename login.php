895ee6d3f8b7f0a0789b53f500fcd0c23ce7e492
aye1306 <chettha13062543@gmail.com>
2021-07-26 01:05:59 +0700

    test commit

--- login.php
+++ login.php
@@ -65,9 +65,6 @@
 <script type="text/javascript">
     $(document).ready(function() {
         localStorage.clear();
-        console.log("GitHub Success!!");
-        console.log("GitHub Success!!");
-        console.log("GitHub Success!!");
         
     });
 </script>
